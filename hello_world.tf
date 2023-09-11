47a5c6e4be4d72741b701790017ed52c15d2ea7f
otc <otc@ontracon.cloud>
2023-09-11 15:21:59 +0200

    fix(hello_world): hello_world.tf ,Managed trough Code

--- hello_world.tf
+++ hello_world.tf
@@ -3,11 +3,16 @@
 # Template: hello_world/hello_world.tf.tpl
 # ---------------------------------------------------------------------------------------------------------------------
 module "hello_world" {
-  source = "git::https://github.com/otc-code/otc-blue-hello.git?ref=v1.0.0"
+  source = "git::https://github.com/otc-code/otc-blue-hello.git?ref=v1.0.1"
   custom = var.custom
 }
 
 output "hello" {
   value       = module.hello_world.hello
+  description = "Only a hello world"
+}
+
+output "new_version" {
+  value       = module.hello_world.new_version
   description = "Only a hello world"
 }
